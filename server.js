17766e822113cf7fc7fa90340de4ba1dad114c54
Javier Dominguez II <Javi7057@gmail.com>
2021-07-25 21:41:39 -0500

    inital files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+// use apiRoutes
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Api server now on port ${PORT}!`)
+});
